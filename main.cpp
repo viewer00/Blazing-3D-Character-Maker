51ed839c9918183a34ba520acbb577fab64d80b2
林冬 <89758913+viewer00@users.noreply.github.com>
2022-07-27 16:01:11 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,33 +5,33 @@
 # define BLOCK_BTN_WIDTH (16 * 2)
 # define ARROW_WIDTH 22
 HWND Win_handle = 0;
-MOUSEMSG m; //½ÓÊÕÊó±êÏûÏ¢µÄ±äÁ¿
+MOUSEMSG m; //æ¥æ”¶é¼ æ ‡æ¶ˆæ¯çš„å˜é‡
 
 void init()
 {
-	int Win_width = 16 * 56; // ´°¿ÚÕæÊµ¿í¶È
-	int Win_height = 16 * 34; // ´°¿ÚÕæÊµ¸ß¶È
+	int Win_width = 16 * 56; // çª—å£çœŸå®å®½åº¦
+	int Win_height = 16 * 34; // çª—å£çœŸå®é«˜åº¦
 	Win_handle = initgraph(Win_width, Win_height);
 }
 
 int main()
 {
 	init();
-	SetWindowTextA(Win_handle, "¼«¹â×Ö¿ÌÉú³É¹¤¾ß£¨Blazing Character maker£©V1.31");
+	SetWindowTextA(Win_handle, "æå…‰ç«‹ä½“å­—ç¬¦åˆ¶ä½œå·¥å…·ï¼ˆBlazing 3D Character makerï¼‰V1.3.1");
 	DrawingBoard dwb(16 * 5, int(16 * 1), 16 * 24, 16 * 32, 2, true);
 	dwb.set_showGrid(true);
 	int start_X = int(16 * 0.5);
 	int start_Y = int(16 * 0.5);
 	int buttonInterval = 16 * 0.5;
-	//Õ¤¸ñ°´Å¥
+	//æ …æ ¼æŒ‰é’®
 	FuntionalButton grid_button(start_X, start_Y, 16 * 4, 16 * 2, '#', 0);
 	bool grid_useful = true;
-	//Çå³ı°´Å¥
+	//æ¸…é™¤æŒ‰é’®
 	FuntionalButton clear_button(start_X, start_Y + (16 * 2 + buttonInterval) * 1, 16 * 4, 16 * 2, 'x', 0);
-	//Í¶Ó°Ô´°´Å¥
+	//æŠ•å½±æºæŒ‰é’®
 	SequentialSelectionalButton origin_button(start_X, start_Y + (16 * 2 + buttonInterval) * 2, 16 * 4, 16 * 2, '&', 0);
 	int show_origin = 0;
-	//»­Ë¢µ¥Ñ¡°´Å¥
+	//ç”»åˆ·å•é€‰æŒ‰é’®
 
 	MultiSelectionlButton brushButton(start_X / 2, start_Y + (16 * 2 + buttonInterval) * 3, 18 * 4, 16 * 8, '#');
 	MultiSelectionlButton lightButton(start_X / 2, start_Y + (16 * 2 + buttonInterval) * 3 + 16 * 8 + buttonInterval, 18 * 4, 18 * 6, '*');
@@ -51,30 +51,30 @@ int main()
 			m = GetMouseMsg();
 			X = m.x;
 			Y = m.y; 
-			// »æÍ¼µÄ°´Å¥Ä£¿é
+			// ç»˜å›¾çš„æŒ‰é’®æ¨¡å—
 			dwb.response(m);
-			// Õ¤¸ñ°´Å¥
+			// æ …æ ¼æŒ‰é’®
 			if (grid_button.response(m))
 			{
 				grid_useful = !grid_useful;
 				dwb.set_showGrid(grid_useful);
 			}
-			// Çå³ı°´Å¥
+			// æ¸…é™¤æŒ‰é’®
 			if (clear_button.response(m))
 			{
 				dwb.clear();
 			}
-			// Í¶Ó°Ô´°´Å¥
+			// æŠ•å½±æºæŒ‰é’®
 			if (origin_button.response(m, show_origin))
 			{
 				dwb.set_showOrigin(show_origin);
 			}
-			// ¹âÏß°´Å¥
+			// å…‰çº¿æŒ‰é’®
 			if (lightButton.response(m, lightID, lightSettings))
 			{
 				dwb.set_projectType(lightID, lightSettings[0]);
 			}
-			// »­Ë¢°´Å¥
+			// ç”»åˆ·æŒ‰é’®
 			if (brushButton.response(m, blockBrushID, brushSettings))
 			{
 				dwb.set_brushType(blockBrushID);
